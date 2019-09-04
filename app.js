abce6968b0dfe25367e46a291b73919d69b811f0
t-fa <thomas.fattah@gmail.com>
2019-09-04 17:14:30 +0000

    Added link to comments form

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
 	seedDB = require("./seeds");
 
-//seedDB();
+seedDB();
 mongoose.set('useNewUrlParser', true);
 mongoose.connect("mongodb://localhost/yelp_camp");
 
@@ -95,6 +96,25 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 			console.log(err);
 		} else {
 			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
 		}
 	});
 });
